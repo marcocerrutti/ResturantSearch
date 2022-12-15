6ef80842028fa4e89ab78562dc47887d05ee4f38
Nwaigwe Uchenna <hermessuffice@gmail.com>
2022-12-15 12:46:15 +0100

    Making reusable hooks

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,41 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
+    const [searchApi, results, errorMessage] = useResults();
 
-    const searchApi = async (searchTerm) => {
-        console.log('Hi there!');
-        try {
-            
-        
-            const response = await yelp.get('/search',
-                {
-                    params: {
-                        limit: 50,
-                        term : searchTerm,
-                        location: 'san jose'
-                    
-                    }
-                });
-            setResults(response.data.businesses);
-        } catch (err) {
-            setErrorMessage('Something went wrong');
-        }
+    const filterResultByPrice = (price) => {
+        //price === '$' || '$$' || '$$$'
+        return results.filter(result => {
+            return result.price === price;
+        });
     };
-
-    //call searchApi when component is first rendered
-    //BAD CODE
-    //searchApi('pasta');
-    useEffect(() => {
-        searchApi('pasta');
-    }, [])
-
+     
     return ( 
         <View>
             <SearchBar term={term}
@@ -44,6 +23,9 @@ const SearchScreen = () => {
             />
             {errorMessage ?<Text>{errorMessage }</Text> : null}
             <Text>We have found {results.length} results</Text>
+            <ResultsList results={filterResultByPrice('$')} title="Cost Effective" />
+            <ResultsList results={filterResultByPrice('$$')} title="Pricier" />
+            <ResultsList results={filterResultByPrice('$$$')} title="Big Spender" />
         </View>
      );
 }
